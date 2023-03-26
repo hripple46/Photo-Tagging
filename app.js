0e68157a7f427f48858cff02265cffb05caff962
Henry Ripple <hripple46@gmail.com>
2023-03-26 15:46:03 -0500

    pulls the user's click coordinates as percentages in relation to the image

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ let getUsersCoordinates = (e) => {
 };
 
 let animalList = document.querySelector(".animal-list"); //selecting the pop up box
+animalList.style.display = "none"; //hiding the pop up box
 
 let togglePopUpBox = (e) => {
   //function to toggle the pop up box
   if (animalList.style.display == "none") {
     // if pop-up box is not displayed, show it
     getUsersCoordinates(e); //calling the function to get the coordinates of the user
+    getCoordinatesAsPercentages(e); //calling the function to get the coordinates of the user as percentages
     animalList.style.display = "block";
     animalList.style.top = `${coordinates.yCoordinate}px`;
     animalList.style.left = `${coordinates.xCoordinate}px`;
@@ -25,4 +27,20 @@ let togglePopUpBox = (e) => {
     animalList.style.display = "none";
   }
 };
-document.body.addEventListener("click", togglePopUpBox);
+document.addEventListener("click", togglePopUpBox);
+
+//this function is to get the coordinates of the user as percentages in relation to the image
+//this allows the resulting coordinates to be compared to the coordinates of the animals
+let getCoordinatesAsPercentages = (e) => {
+  let image = document.querySelector("img");
+  let imageWidth = image.clientWidth;
+  let imageHeight = image.clientHeight;
+  let x = e.clientX - image.offsetLeft;
+  let y = e.clientY - image.offsetTop;
+  let xPercentage = (x / imageWidth) * 100;
+  let yPercentage = (y / imageHeight) * 100;
+  console.log({
+    xCoordinatePercentage: xPercentage,
+    yCoordinatePercentage: yPercentage,
+  });
+};
